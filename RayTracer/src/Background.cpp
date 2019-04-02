42ca8a5cf962e141d497c083e9b75af675996d88
Giovanni Franco De Paula Rosario <giovannirosario@A307-36L.imd.local>
2019-04-02 08:13:18 -0300

    Allowing less than 4 colors input Interpolation

--- RayTracer/src/Background.cpp
+++ RayTracer/src/Background.cpp
@@ -14,9 +14,12 @@ void Background::add_color(Color color){
 
 Color Background::get_pixel(float x, float y){
     if(type == "interpolation") {
-        int r = colors[0].get_r() * (1-x)*(1-y) + colors[1].get_r() * x * (1-y) + colors[2].get_r() * x * y + colors[3].get_r() * (1-x) * y;
-        int g = colors[0].get_g() * (1-x)*(1-y) + colors[1].get_g() * x * (1-y) + colors[2].get_g() * x * y + colors[3].get_g() * (1-x) * y;
-        int b = colors[0].get_b() * (1-x)*(1-y) + colors[1].get_b() * x * (1-y) + colors[2].get_b() * x * y + colors[3].get_b() * (1-x) * y;
+        while(colors.size() != 4) {
+            colors.push_back(colors.back());
+        }
+        int r = colors[0].get_r() * (1-x)*(1-y) + colors[1].get_r() * (1-x) * y + colors[2].get_r() * x * y + colors[3].get_r() * x * (1-y);
+        int g = colors[0].get_g() * (1-x)*(1-y) + colors[1].get_g() * (1-x) * y + colors[2].get_g() * x * y + colors[3].get_g() * x * (1-y);
+        int b = colors[0].get_b() * (1-x)*(1-y) + colors[1].get_b() * (1-x) * y + colors[2].get_b() * x * y + colors[3].get_b() * x * (1-y);
         return Color(r,g,b);
     }
 };
